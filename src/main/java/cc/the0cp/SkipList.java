c651608a21f312adf557e85d975824da9db58552
the0cp <ccooperr2005@gmail.com>
2025-04-07 14:48:19 +0800

    add insert node method

--- src/main/java/cc/the0cp/SkipList.java
+++ src/main/java/cc/the0cp/SkipList.java
@@ -1,6 +1,7 @@
 package cc.the0cp;
 
 import java.io.*;
+import java.security.Key;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -85,7 +86,7 @@ public class SkipList<K extends Comparable<K>, V>{
         while(rand.nextBoolean()){
             level++;
         }
-        return level;
+        return Math.min(level, MAX_LEVEL);
     }
 
     /**
@@ -96,14 +97,48 @@ public class SkipList<K extends Comparable<K>, V>{
         return this.nodeCnt;
     }
 
+    /**
+     * insert specific node
+     * @param key   key of the node
+     * @param value value of the node
+     * @return  true if node is successfully inserted
+     */
     public synchronized boolean insert(K key, V value){
         Node<K, V> cur = this.header;
-        ArrayList<Node<K, V>> update = new ArrayList<>(Collections.nCopies(MAX_LEVEL + 1, null));
+        ArrayList<Node<K, V>> updateTable = new ArrayList<>(Collections.nCopies(MAX_LEVEL + 1, null));
 
-        for(int i = this.curLevel; i >= 0; i--){
+        for(int i = this.curLevel; i >= 0; i--){    // get the last lower key
             while(cur.forward.get(i) != null && cur.forward.get(i).getKey().compareTo(key) < 0){
                 cur = cur.forward.get(i);
             }
+            updateTable.set(i, cur);
+        }
+
+        cur = cur.forward.getFirst();   // lowest level, get(0)
+
+        if(cur != null && cur.getKey().compareTo(key) == 0){    // same key, replace value
+            cur.setValue(value);
+            return true;
+        }
+
+        int randLevel = randomLevel();
+
+        if(cur == null || cur.getKey().compareTo(key) != 0){    // different key, insert node
+            if(randLevel > curLevel){
+                for(int i = curLevel + 1; i < randLevel + 1; i++){
+                    updateTable.set(i, header);
+                }
+                curLevel = randLevel;
+            }
+
+            Node<K, V> insertNode = createNode(key, value, randLevel);
+
+            for(int i = 0; i <= randLevel; i++){    // insert node and index
+                insertNode.forward.set(i, updateTable.get(i).forward.get(i));
+                updateTable.get(i).forward.set(i, insertNode);
+            }
+            nodeCnt++;
+            return true;
         }
         return false;
     }
